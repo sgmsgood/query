a00982ca11cb37937ee7cd301adaaa7301c05cf6
sgmsgood <45481968+sgmsgood@users.noreply.github.com>
2019-01-29 17:53:57 +0900

    commit

---
+++ lunch/lunch_order_select.sql
@@ -0,0 +1,14 @@
+create or replace  lunch_order_select(
+order_name in varchar2, i_phone in varchar2, cur_order_list out sys_refcursor)
+is 
+        
+begin
+ open cur_order_list for 
+ select l.lunch_name, to_char(o.order_date,'yyyy-mm-dd day hh24:mi:ss') order_date, o.quan 
+ from ordering o, lunch l 
+ where o.lunch_code=l.lunch_code and 
+ 		 o.phone=i_phone
+ order by o.order_date desc;
+  
+end;
+/
