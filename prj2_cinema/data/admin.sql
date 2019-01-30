b775100183484ebdf9288bd66963ca658913df2b
sgmsgood <45481968+sgmsgood@users.noreply.github.com>
2019-01-30 18:33:01 +0900

    commit

---
+++ prj2_cinema/data/admin.sql
@@ -0,0 +1,11 @@
+insert into admin (admin_id, name, password, hiredate) values ('sso','¹Ú¼Ò¿µ',1234,'2018-05-24');
+
+insert into admin (admin_id, name, password, hiredate) values ('hee','±èÈñÃ¶',1234,'2018-05-24');
+
+insert into admin (admin_id, name, password, hiredate) values ('jin','³ëÁø°æ',1234,'2008-06-01');
+
+insert into admin (admin_id, name, password, hiredate) values ('young','¹Ú¿µ¹Î',1234,'2013-07-31');
+
+commit;
+
+select * from ADMIN;
---
+++ prj2_cinema/data/member.sql
@@ -0,0 +1,35 @@
+insert into member (member_id, password, name, birthdate, phone, membership, hold_point, acc_point, input_date)
+values('roma_emo','jmjmhaha','한지민','19820511','017-4567-3241','g', 45000, 55000, '2011-03-03');
+
+insert into member (member_id, password, name, birthdate, phone, membership, hold_point, acc_point, input_date)
+values('yesreal82','rice','손예진','19820303','016-7541-3546','s', 70000, 270000, '2016-05-04');
+
+insert into member (member_id, password, name, birthdate, phone, membership, hold_point, acc_point, input_date)
+values('trust','luvkei','김주영','19800711','010-7814-6572','g', 38000, 210000, '2016-07-27');
+
+insert into member (member_id, password, name, birthdate, phone, membership, hold_point, acc_point, input_date)
+values('doctor_seul','wouldyou123','강예서','20010721','010-7878-1354','s', 10000, 10000, '2011-12-31');
+
+insert into member (member_id, password, name, birthdate, phone, membership, hold_point, acc_point, input_date)
+values('seojin2','agalheadshot','곽미향','19750412','010-4735-2141','s',10000, 10000, '2011-12-31');
+
+insert into member (member_id, password, name, birthdate, phone, membership, hold_point, acc_point, input_date)
+values('harvard17','!breakpyramid','차세리','19980612','010-7878-0101','v',20000, 310000, '2008-06-12');
+
+insert into member (member_id, password, name, birthdate, phone, membership, hold_point, acc_point, input_date)
+values('pyramid_top','chapagook17','차민혁','1973','010-7541-3456','g', 20000, 120000, '2019-01-20');
+
+insert into member (member_id, password, name, birthdate, phone, membership, hold_point, acc_point, input_date)
+values('jjini83','eomama!3546','진진희','19831212','010-4875-3546','v', 50000, 450000, '2009-10-12');
+
+insert into member (member_id, password, name, birthdate, phone, membership, hold_point, acc_point, input_date)
+values('mamasang','sorryhyena0','강준상','19730909','017-5784-3535','g', 45000, 150000, '2015-06-11');
+
+insert into member (member_id, password, name, birthdate, phone, membership, hold_point, acc_point, input_date)
+values('stealjam','bin22222','강예빈','20070401','010-4577-4586','s', 10000, 10000, '2011-12-31');
+
+commit;
+
+-- 회원, 관리자,
+select * from member;
+
